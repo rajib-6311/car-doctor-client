f468fc557f5fa44d4407b301e252bd84a3d3c787
rajibhossen <mhossen6311@gmail.com>
2024-06-05 23:22:18 +0600

    Services added

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const ServiceCard = ({ service }) => {
-  const { title, service_id, img, price, description } = service;
+  const {_id, title,img,price,} = service;
   return (
     <div className="card h-96 w-96 bg-base-100 shadow-xl ">
       <figure>
@@ -12,7 +14,9 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title text-green-500">{title}</h2>
               {/* <p>{description}</p> */}
               <p className="text-orange-500">Price : { price}</p>
-        
+        <Link to={`/checkout/${_id}`}>
+        <button className="btn btn-error">Book Now</button>
+        </Link>
       </div>
     </div>
   );
